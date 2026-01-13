2728b69d1f78ba8c0421c642ca7c26bacd36f3bf
unknown <abdullahalsayed087@gmail.com>
2026-01-13 10:18:46 +0600

    DbContext Added

---
+++ Estatia/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Estatia.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Estatia.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Property> Properties { get; set; }
+
+
+
+    public DbSet<Division> Divisions { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Area> Areas { get; set; }
+}
