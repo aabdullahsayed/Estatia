807c046c39e52b7eab988fd56e125a1ac7dfb8a3
unknown <abdullahalsayed087@gmail.com>
2026-01-15 13:50:44 +0600

    Add Data Annotation

--- Estatia/Models/Property.cs
+++ Estatia/Models/Property.cs
@@ -12,13 +12,32 @@ public class Property {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
-    
+    [Required]
+    [StringLength(100)]
     public string Title { get; set; }
+    
+    [Required]
+    [Range(1,double.MaxValue, ErrorMessage = "Price must be greater than zero")]
     public decimal Price { get; set; }
+    
+    [Required]
     public ListingType Type { get; set; }
+    
+    [Required]
     public string Division { get; set; }
+    
+    [Required]
     public string City { get; set; }
+    
+    [Required]
     public string Area { get; set; }
-    public string ImageUrl { get; set; }
+    
+    public string? ImageUrl { get; set; }
+    
+    [NotMapped]
+    [Required(ErrorMessage = "Upload a Photo")]
+    [Display(Name = "Upload Image")]
+    public IFormFile ImageFile { get; set; }
+    
     
 }
