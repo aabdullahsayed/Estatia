1b95c09e701bc94f2933e938814827b2abb9371d
unknown <abdullahalsayed087@gmail.com>
2026-01-15 13:43:44 +0600

    Update Property model

--- Estatia/Models/Property.cs
+++ Estatia/Models/Property.cs
@@ -1,5 +1,24 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public enum ListingType
+{
+    Rent,Sell
+}
 public class Property {
+    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    
+    
     public string Title { get; set; }
     public decimal Price { get; set; }
+    public ListingType Type { get; set; }
+    public string Division { get; set; }
+    public string City { get; set; }
+    public string Area { get; set; }
+    public string ImageUrl { get; set; }
+    
 }
