832cd1c08036852d466516706420417a59588eb2
unknown <abdullahalsayed087@gmail.com>
2026-01-18 11:49:08 +0600

    Add View Model Admin

---
+++ Estatia/Models/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Estatia.Models;
+namespace Estatia.Models.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        
+        public int TotalUsers { get; set; }
+        public int TotalAgents { get; set; }
+        public int TotalProperties { get; set; }
+
+        
+        public List<Agent> Agents { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
