80a44113ac00c3dd9f0854f38c1c7aeb13dae01a
unknown <abdullahalsayed087@gmail.com>
2026-01-18 11:27:23 +0600

    Add Agent Model

---
+++ Estatia/Models/Agent.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Estatia.Models;
+
+public class Agent
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; } 
+
+    
+    public ICollection<Property>? Properties{ get; set; } 
+}
